f002d96842c7c0b1ffae3c2b75cdfb6e00f21dcb
xPaddy <118140299+xPaddy@users.noreply.github.com>
2024-03-11 16:47:54 +0100

    Solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, rep) {
+  let result = "";
+  if (rep < 0) {
+    result = "ERROR";
+  } else if (string.length > 0) {
+    while (rep > 0) {
+      result += string;
+      rep--;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
