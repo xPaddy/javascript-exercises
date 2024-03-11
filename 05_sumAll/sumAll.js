38bfee93480095ed9a552ac4a3301b544c7a1d6e
xPaddy <118140299+xPaddy@users.noreply.github.com>
2024-03-11 19:06:08 +0100

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  
+  if (min < 0 || max < 0 || !Number.isInteger(min) || !Number.isInteger(max)) {
+    return "ERROR";
+  }
+  
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+  
+  let sum = 0;
 
+  for (let i = min; i <= max; i++) {
+    sum += i;    
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
