b7a14fcf70a3b428530f9d26bdb5122250d28c46
xPaddy <118140299+xPaddy@users.noreply.github.com>
2024-03-11 18:23:17 +0100

    Solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...del) {
+   const resultArr = [];
+      for (const elem of array) {
+         if (!del.includes(elem)) {
+            resultArr.push(elem);
+         }
+      }
+   return resultArr;
 };
 
 // Do not edit below this line
